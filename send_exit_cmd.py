d0ffd33560cf7c9c4acd5e79578a4783233df3b2
gagahan <hofmaier@gmx.ch>
2017-04-08 09:51:29 +0200

    seperated msg module from iec module

--- send_exit_cmd.py
+++ send_exit_cmd.py
@@ -2,4 +2,4 @@ import iec
 import msg
 
 meter = iec.MeterDevice(iec.COM_7E1_9600(port='/dev/ttyUSB0'))
-meter.send_msg(msg.Break())
+meter.send_receive(msg.Break())
--- test.py
+++
@@ -1,5 +0,0 @@
-import iec_com
-
-
-print('test')
-print(iec_com.cln_str('\x021.8.0(00000000.057*kWh)\x03Y'))
